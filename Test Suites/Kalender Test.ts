18ece58ebecbaf992ecb6c907bfc49fbafce1e30
Vaze09 <fikriabdillah2010@gmail.com>
2025-06-20 17:32:21 +0700

    main

---
+++ Test Suites/Kalender Test.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Kalender Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>350f18fd-6d31-42a3-89be-83fa79d91e6d</testSuiteGuid>
+</TestSuiteEntity>
